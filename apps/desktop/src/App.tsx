886f5e4dc386b1740b823690a424aa3816499f2c
Nikita Povalishev <np@skl.vc>
2023-12-18 11:18:28 +0300

    Run desktop app with ui-kit lib

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@ui-kit/components/button/Button';
+
 console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`);
 
 function App() {
-  return <div className='App'>Electron App</div>;
+  return (
+    <div className='App'>
+      Electron App
+      <Button className='min-w-[175px]'>Start</Button>
+    </div>
+  );
 }
 
 export default App;
--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig(({ command }) => {
   return {
     resolve: {
       alias: {
-        '@': path.join(__dirname, 'src')
+        '@': path.join(__dirname, 'src'),
+        '@ui-kit': path.join(__dirname, '../../libs/ui-kit/')
       },
     },
     plugins: [
